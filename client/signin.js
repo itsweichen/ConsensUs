1e0fbd626ffa42672d0b14b2bd717ed5247e1601
Weichen Liu <kevbtc@gmail.com>
2016-03-22 10:05:10 -0700

    decisions page layout

--- client/signin.js
+++ client/signin.js
@@ -1,12 +1,11 @@
 Template.signin.events({
     'submit form': function(event){
         event.preventDefault();
-        var username = $('#inputUsername').val();
+        var userName = $('#inputUsername').val();
         var password = $('#inputPassword').val();
-        Meteor.loginWithPassword(username, password, function(err){
-            if(err){
-                console.log(err);
-            }
+        Accounts.createUser({
+            email: username,
+            password: password
         });
     }
 });
